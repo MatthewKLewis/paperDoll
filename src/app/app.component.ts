2eae64b8f1381189ee7007981286e4f7374c76ec
MatthewKLewis <matthew.knight.lewis@gmail.com>
2021-09-21 00:21:22 -0400

    jitter annoying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,222 @@
-import {Component} from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+
+interface Attack {
+  damageType: string;
+  damage: number;
+  armorPiercing: number;
+}
+
+interface Defense {
+  name: string;
+  type: string;
+}
 
 interface Part {
-  name: string,
-  type: string,
-  weight: number,
-  powerConsumption: number,
-  powerProduction?: number,
-  description: string,
-  cost?: number,
+  name: string;
+  type: string;
+  quality: string;
+  weight: number;
+  powerConsumption: number;
+  description: string;
+
+  cost?: number;
+  powerProduction?: number;
+  attack?: Attack;
+  defense?: Defense;
+}
+
+function sortByPartType(partA: Part, partB: Part): number {
+  var partAValue = 0;
+  var partBValue = 0;
+
+  switch (partA.type) {
+    case 'sensor':
+      partAValue = 5;
+      break;
+    case 'core':
+      partAValue = 4;
+      break;
+    case 'rarm':
+      partAValue = 3;
+      break;
+    case 'larm':
+      partAValue = 3;
+      break;
+    case 'legs':
+      partAValue = 2;
+      break;
+    case 'misc':
+      partAValue = 1;
+      break;
+    default:
+      partAValue = 0;
+      break;
+  }
+
+  switch (partB.type) {
+    case 'sensor':
+      partBValue = 5;
+      break;
+    case 'core':
+      partBValue = 4;
+      break;
+    case 'rarm':
+      partAValue = 3;
+      break;
+    case 'larm':
+      partAValue = 3;
+      break;
+    case 'legs':
+      partBValue = 2;
+      break;
+    case 'misc':
+      partBValue = 1;
+      break;
+    default:
+      partBValue = 0;
+      break;
+  }
+
+  return partBValue - partAValue;
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
-  errorMessage: string = ''
+  errorMessage: string = '';
+  partFilter: string = '';
 
   inactiveParts: Array<Part> = [
-    {name: 'EXPERT HEAD', type: 'sensor', weight: 100, description: 'a mecha part', powerConsumption: 10},
-    {name: 'EXPERT CORE', type: 'core', weight: 100, description: 'a mecha part', powerConsumption: 10, powerProduction: 300},
-    {name: 'EXPERT L ARM', type: 'larm', weight: 100, description: 'a mecha part', powerConsumption: 10},
-    {name: 'ULTIMATE R ARM', type: 'rarm', weight: 100, description: 'a mecha part', powerConsumption: 10},
-    {name: 'SWIFT LEGS', type: 'legs', weight: 100, description: 'a mecha part', powerConsumption: 10},
-    {name: 'HEAVY LEGS', type: 'legs', weight: 100, description: 'a mecha part', powerConsumption: 10},
-    {name: 'SHOULDER MOUNTED ROCKET SYSTEM', type: 'shoulders', weight: 100, description: 'a mecha part', powerConsumption: 10},
+    {
+      name: 'EXPERT HEAD',
+      type: 'sensor',
+      quality: 'performance',
+      weight: 100,
+      description: 'an advanced sensor array',
+      powerConsumption: 10,
+    },
+    {
+      name: 'EXPERT CORE',
+      type: 'core',
+      quality: 'performance',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 10,
+      powerProduction: 300,
+    },
+    {
+      name: 'EXPERT Left ARM',
+      type: 'larm',
+      quality: 'performance',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 10,
+      attack: { damageType: 'melee', damage: 10, armorPiercing: 0 },
+    },
+    {
+      name: 'ULTIMATE Right ARM',
+      type: 'rarm',
+      quality: 'elite',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 10,
+    },
+    {
+      name: 'SWIFT LEGS',
+      type: 'legs',
+      quality: 'master',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 10,
+    },
+    {
+      name: 'HEAVY LEGS',
+      type: 'legs',
+      quality: 'master',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 10,
+    },
+    {
+      name: 'SHOULDER MOUNTED ROCKET SYSTEM',
+      type: 'rshoulder',
+      quality: 'performance',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 10,
+    },
+    {
+      name: 'SHOULDER MOUNTED LASER SYSTEM',
+      type: 'lshoulder',
+      quality: 'performance',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 10,
+    },
   ];
-  
+
   activeParts: Array<Part> = [
-    {name: 'Standard Head', type: 'sensor', weight: 100, description: 'a mecha part', powerConsumption: 10},
-    {name: 'Standard Core', type: 'core', weight: 100, description: 'a mecha part', powerConsumption: 20, powerProduction: 200},
-    {name: 'Standard Left Arm', type: 'larm', weight: 100, description: 'a mecha part', powerConsumption: 30},
-    {name: 'Standard Right Arm', type: 'rarm', weight: 100, description: 'a mecha part', powerConsumption: 40},
-    {name: 'Standard Legs', type: 'legs', weight: 100, description: 'a mecha part', powerConsumption: 50},
+    {
+      name: 'Junk Head',
+      type: 'sensor',
+      quality: 'junk',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 10,
+    },
+    {
+      name: 'Standard Core',
+      type: 'core',
+      quality: 'stock',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 20,
+      powerProduction: 200,
+    },
+    {
+      name: 'Standard Right Arm',
+      type: 'rarm',
+      quality: 'stock',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 30,
+    },
+    {
+      name: 'Junk Left Arm',
+      type: 'larm',
+      quality: 'junk',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 40,
+    },
+    {
+      name: 'Standard Legs',
+      type: 'legs',
+      quality: 'stock',
+      weight: 100,
+      description: 'a mecha part',
+      powerConsumption: 50,
+    },
   ];
 
   drop(event: CdkDragDrop<any[]>) {
-    //log item to be moved:
-    var newContainerId = event.container.id
-    console.log(newContainerId)
-
-    var itemToMove = event.previousContainer.data[event.previousIndex]
-    console.log(itemToMove)
+    var newContainerId = event.container.id;
+    var itemToMove = event.previousContainer.data[event.previousIndex];
 
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else if (newContainerId === 'activePartsElement') {
       var identicalPartTypeFound = false;
       var identicalPartTypeIndex = -1;
@@ -56,47 +225,70 @@ export class AppComponent {
           identicalPartTypeIndex = i;
           identicalPartTypeFound = true;
           break;
-        }        
+        }
       }
       if (identicalPartTypeFound) {
-        console.log("Core Already Added.")
         transferArrayItem(
           event.container.data,
           event.previousContainer.data,
           identicalPartTypeIndex,
-          event.previousContainer.data.length - 1);
+          event.previousContainer.data.length - 1
+        );
         transferArrayItem(
           event.previousContainer.data,
           event.container.data,
           event.previousIndex,
-          event.currentIndex);
+          event.currentIndex
+        );
       } else {
         transferArrayItem(
           event.previousContainer.data,
           event.container.data,
           event.previousIndex,
-          event.currentIndex);
+          event.currentIndex
+        );
       }
     } else if (newContainerId === 'inactivePartsElement') {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex);
+        event.currentIndex
+      );
     }
+
+    //and sort
+    this.inactiveParts.sort((partA, partB) => {
+      return sortByPartType(partA, partB);
+    });
+    this.activeParts.sort((partA, partB) => {
+      return sortByPartType(partA, partB);
+    });
+  }
+
+  filterInactiveParts() {
+    return this.inactiveParts.filter((part) => {
+      return !this.partFilter || part.type.includes(this.partFilter);
+    });
   }
 
   getImagePath(partType: string) {
     switch (partType) {
-      case 'head':
-        return '../assets/images/head.webp'    
+      case 'sensor':
+        return '../assets/images/Head.png';
+      case 'core':
+        return '../assets/images/Core.png';
+      case 'legs':
+        return '../assets/images/Legs.png';
+      case 'misc':
+        return '#';
       default:
-        return '../assets/images/head.webp'
+        return '../assets/images/LeftArm.png';
     }
   }
 
   getTotalWeight() {
-    var totalWeight = 0
+    var totalWeight = 0;
     for (let i = 0; i < this.activeParts.length; i++) {
       totalWeight += this.activeParts[i].weight;
     }
@@ -104,7 +296,7 @@ export class AppComponent {
   }
 
   getTotalPowerProduction() {
-    var totalPP = 0
+    var totalPP = 0;
     for (let i = 0; i < this.activeParts.length; i++) {
       totalPP += this.activeParts[i].powerProduction || 0;
     }
@@ -112,19 +304,19 @@ export class AppComponent {
   }
 
   getTotalPowerConsumption() {
-    var totalPC = 0
+    var totalPC = 0;
     for (let i = 0; i < this.activeParts.length; i++) {
       totalPC += this.activeParts[i].powerConsumption;
     }
     return totalPC;
   }
 
-  isMechaComplete(): boolean { //TO-DO
+  isMechaComplete(): boolean {
+    //TO-DO
     if (this.activeParts.length < 5) {
       return false;
     } else {
       return true;
     }
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DragDropModule } from '@angular/cdk/drag-drop'; 
-
+import { MatMenuModule } from '@angular/material/menu'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DragDropModule
+    DragDropModule,
+    MatMenuModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
